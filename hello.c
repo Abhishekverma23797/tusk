d69991210897419de1f720c1ef4509e0c622beb9
Abhishekverma23797 <39663356+Abhishekverma23797@users.noreply.github.com>
2020-04-02 00:24:12 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,11 +2,14 @@
 #include<conio.h>
 void main()
 {
-  int num1,num2,sum;
+  int num1,num2,sum,sub;
   printf("Enter two number");
   scanf("%d %d",&num1,&num2);
   sum=num1+num2;
   printf("Sum is %d=",sum);
+  printf("Subtraction is\n");
+  sub=num1-num2;
+  printf("subtraction is %d=",sub);
   printf("welcome to git hub\n");
   getch();
  } 
