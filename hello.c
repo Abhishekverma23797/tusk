c9475adfe48cdd1584544f9dbba55d031dd64223
Abhishekverma23797 <39663356+Abhishekverma23797@users.noreply.github.com>
2020-04-02 00:44:20 +0530

    div.c

--- hello.c
+++ hello.c
@@ -2,11 +2,13 @@
 #include<conio.h>
 void main()
 {
-  int num1,num2,sum;
+  int num1,num2,sum,div;
   printf("Enter two number");
   scanf("%d %d",&num1,&num2);
   sum=num1+num2;
   printf("Sum is %d=",sum);
+  div=num1+num2;
+  printf("divison is=%d",div);
   printf("welcome to git hub\n");
   getch();
  } 
