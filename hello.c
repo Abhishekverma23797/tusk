23f31c0f006862e9c4fa00f5917100f7b555e05f
Abhishekverma23797 <39663356+Abhishekverma23797@users.noreply.github.com>
2020-04-02 00:18:58 +0530

    hellosum.c

--- hello.c
+++ hello.c
@@ -3,6 +3,10 @@
 void main()
 {
   int num1,num2,sum;
+  printf("Enter two number");
+  scanf("%d %d",&num1,&num2);
+  sum=num1+num2;
+  printf("Sum is %d=",sum);
   printf("welcome to git hub\n");
   getch();
  } 
